baaf627a15643f66d981550e3d843ef5dbb95566
Damien MATHIEU <damien.mathieu@etu.umontpellier.fr>
2025-04-13 17:37:27 +0200

    fix: ts build

---
+++ src/ldrs.d.ts
@@ -0,0 +1,16 @@
+declare namespace JSX {
+  interface IntrinsicElements {
+    'l-ring': React.DetailedHTMLProps<React.HTMLAttributes<HTMLElement>, HTMLElement> & {
+      size?: string;
+      stroke?: string;
+      'bg-opacity'?: string;
+      speed?: string;
+      color?: string;
+    };
+    'l-leapfrog': React.DetailedHTMLProps<React.HTMLAttributes<HTMLElement>, HTMLElement> & {
+      size?: string;
+      speed?: string;
+      color?: string;
+    };
+  }
+}
